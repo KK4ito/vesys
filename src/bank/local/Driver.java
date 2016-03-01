0d525334536653e6b95fc92c04c8d02b6068879a
Kevin Lien <k4ito@outlook.com>
2016-03-01 10:35:48 +0100

    passing all unit tests (local instance)

--- src/bank/local/Driver.java
+++ src/bank/local/Driver.java
@@ -42,7 +42,9 @@ public class Driver implements bank.BankDriver {
 		public Set<String> getAccountNumbers() {
 			Set<String> lstAccountNumbers = new HashSet<>();
 			for(Account a : this.accounts.values()){
-				lstAccountNumbers.add(a.number);
+				if(a.isActive()){
+					lstAccountNumbers.add(a.number);
+				}
 			}
 			return lstAccountNumbers;
 		}
@@ -51,13 +53,17 @@ public class Driver implements bank.BankDriver {
 		public String createAccount(String owner) {
 			Account a = new Account(owner, String.valueOf(accNumber));
 			accNumber++;
-			this.accounts.put(owner, a);
-			return owner;
+			
+			this.accounts.put(a.number, a);
+			return a.number;
 		}
 
 		@Override
 		public boolean closeAccount(String number) {
 			if(this.accounts.get(number) == null){
+				return false;
+			}
+			else if(this.accounts.get(number).getBalance() != 0 || !this.accounts.get(number).isActive()){
 				return false;
 			}
 			Account a = this.accounts.get(number);
@@ -74,7 +80,7 @@ public class Driver implements bank.BankDriver {
 		public void transfer(bank.Account from, bank.Account to, double amount)
 				throws IOException, InactiveException, OverdrawException {
 			if(from.isActive() == false && to.isActive()== false ) throw new InactiveException();
-			if(from.getBalance() < amount)throw new OverdrawException();
+			if(from.getBalance() < amount || amount < 0)throw new OverdrawException();
 			
 			from.withdraw(amount);
 			to.deposit(amount);
@@ -115,13 +121,14 @@ public class Driver implements bank.BankDriver {
 
 		@Override
 		public void deposit(double amount) throws InactiveException {
-			if(this.active == false) throw new InactiveException();
+			if(this.active == false || amount < 0) throw new InactiveException();
+		
 			this.balance += amount;
 		}
 
 		@Override
 		public void withdraw(double amount) throws InactiveException, OverdrawException {
-			if(this.active == false) throw new InactiveException();
+			if(this.active == false || amount < 0) throw new InactiveException();
 			if(this.balance < amount)throw new OverdrawException();
 			this.balance -= amount;
 		}
